d0d27999548fd344e9eb49636bf102e905203df8
AIMKK <a_5210@126.com>
2019-08-28 17:26:16 +0800

    add file

---
+++ iRMS/ViewModel/LoginVM.cs
@@ -0,0 +1,92 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using MahApps.Metro.Controls.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace iRMS
+{
+    public class LoginVM : ViewModelBase
+    {
+
+        private string irmsVersion = "版本:V1.1.1.1";
+        public string IrmsVersion
+        {
+            get { return irmsVersion; }
+            set { irmsVersion = value; }
+        }
+
+        private string loginIP = "网络位置:192.168.122.246";
+        public string LoginIP
+        {
+            get { return loginIP; }
+            set { loginIP = value; }
+        }
+
+        private string dbIP = "数据服务:192.168.122.246";
+        public string DBIP
+        {
+            get { return dbIP; }
+            set { dbIP = value; }
+        }
+
+
+        private string lang = "CHS";
+        public string Lang
+        {
+            get { return lang; }
+            set { Set(ref lang, value); }
+        }
+
+        private string userCode = "abc";
+        public string UserCode
+        {
+            get { return userCode; }
+            set { Set(ref userCode, value); }
+        }
+
+
+
+        public ICommand LoginCommand { get; set; }
+
+        public RelayCommand<object> FormLoad { get; set; }
+
+        public LoginVM()
+        {
+            LoginCommand = new RelayCommand(loginBtnClick);
+            FormLoad = new RelayCommand<object>(formLoad);
+
+        }
+
+        private void loginBtnClick()
+        {
+            MessageBox.Show("login btn clicked");
+            //
+            testweakref.starttest();
+        }
+
+        private void formLoad(object sender)
+        {
+            Window win = sender as Window;
+            win.Dispatcher.BeginInvoke((Action)delegate ()
+                {
+                    MessageBox.Show("formLoad");
+                }, null);
+            //System.Threading.Thread th = new System.Threading.Thread(() =>
+            //{
+            //    win.Dispatcher.BeginInvoke((Action)delegate ()
+            //    {
+            //        MessageBox.Show("formLoad");
+            //    }, null);
+
+            //});
+            //th.IsBackground = true;
+            //th.Start();
+        }
+    }
+}
---
+++ iRMS/Views/Login.xaml.cs
@@ -0,0 +1,77 @@
+﻿using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace iRMS
+{
+    /// <summary>
+    /// Login.xaml 的交互逻辑
+    /// </summary>
+    public partial class Login : MetroWindow//Window
+    {
+        //LoginVM vm = new LoginVM(DialogCoordinator.Instance);
+        //
+        public Login()
+        {
+            InitializeComponent();
+            //var grid = this.Content as Grid ;
+
+
+            ////
+            //Button btn = new Button() { Width = grid.Width, Height = grid.Height };
+            //btn.SetValue(Grid.RowProperty, 0);
+            //btn.SetValue(Grid.ColumnProperty, 0);
+            //btn.SetValue(Grid.ColumnSpanProperty, grid.RowDefinitions.Count);
+            //btn.SetValue(Grid.RowSpanProperty, grid.ColumnDefinitions.Count);
+            //btn.Opacity = 0.0;
+
+            ////
+            //grid.Children.Add(btn);
+
+            //var grid = this.Content as Grid;
+            //var v = grid.Children;
+            //var control = this.FindName("loginUsercode") as TextBox ;
+            //control.Height=40;
+            //control.ClearValue(FrameworkElement.HeightProperty);
+
+            this.Loaded += Login_Loaded;
+
+        }
+
+        private void Login_Loaded(object sender, RoutedEventArgs e)
+        {
+            //this.Dispatcher.BeginInvoke((Action)delegate ()
+            //      {
+            //          MessageBox.Show("formLoad2");
+            //      }, null);
+
+            //System.Threading.Thread th = new System.Threading.Thread(() =>
+            //  {
+            //      this.Dispatcher.BeginInvoke((Action)delegate()
+            //      {
+            //          MessageBox.Show("formLoad2");
+            //      },null);
+
+            //  });
+            //th.IsBackground = true;
+            //th.Start();
+        }
+
+        private void MetroWindow_Initialized(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
