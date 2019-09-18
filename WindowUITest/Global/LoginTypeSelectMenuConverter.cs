15789cf6634ec050fd9637bb59ab240cc26f0aa0
AIMKK <a_5210@126.com>
2019-09-18 15:42:18 +0800

    bind templateparent

---
+++ WindowUITest/Global/LoginTypeSelectMenuConverter.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace WindowUITest
+{
+    public class LoginTypeSelectMenuConverter : IMultiValueConverter
+    {
+        class stateEnum
+        {
+            public const string Normal = "Normal";
+            public const string MouseOver = "MouseOver";
+            public const string Selected = "Selected";
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="values"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            string imagePath = "";
+            object result = null;
+            try
+            {
+                if (values != null && values.Length >= 2)
+                {
+                    string typeMenu = values[0] as string;
+                    string state = values[1] as string;
+                    //
+                    #region convertToImagePath
+
+                    if (string.Equals(typeMenu, "RoadShow", StringComparison.OrdinalIgnoreCase))
+                    {
+                        if (string.Equals(state, stateEnum.Normal, StringComparison.OrdinalIgnoreCase))
+                        {
+                            imagePath = "pack://application:,,,/Images/btn_RS_CHS.png";
+                        }
+                        else if (string.Equals(state, stateEnum.MouseOver, StringComparison.OrdinalIgnoreCase))
+                        {
+                            imagePath = "pack://application:,,,/Images/btn_RS_CHS_over.png";
+                        }
+                        else if (string.Equals(state, stateEnum.Selected, StringComparison.OrdinalIgnoreCase))
+                        {
+                            imagePath = "pack://application:,,,/Images/btn_RS_CHS_click.png";
+                        }
+                    }
+                    else if (string.Equals(typeMenu, "SHOP", StringComparison.OrdinalIgnoreCase))
+                    {
+                        if (string.Equals(state, stateEnum.Normal, StringComparison.OrdinalIgnoreCase))
+                        {
+                            imagePath = "pack://application:,,,/Images/btn_SHOP_CHS.png";
+                        }
+                        else if (string.Equals(state, stateEnum.MouseOver, StringComparison.OrdinalIgnoreCase))
+                        {
+                            imagePath = "pack://application:,,,/Images/btn_SHOP_CHS_over.png";
+                        }
+                        else if (string.Equals(state, stateEnum.Selected, StringComparison.OrdinalIgnoreCase))
+                        {
+                            imagePath = "pack://application:,,,/Images/btn_SHOP_CHS_click.png";
+                        }
+                    }
+                    else if (string.Equals(typeMenu, "OFFICE", StringComparison.OrdinalIgnoreCase))
+                    {
+                        if (string.Equals(state, stateEnum.Normal, StringComparison.OrdinalIgnoreCase))
+                        {
+                            imagePath = "pack://application:,,,/Images/btn_WH_CHS.png";
+                        }
+                        else if (string.Equals(state, stateEnum.MouseOver, StringComparison.OrdinalIgnoreCase))
+                        {
+                            imagePath = "pack://application:,,,/Images/btn_WH_CHS_over.png";
+                        }
+                        else if (string.Equals(state, stateEnum.Selected, StringComparison.OrdinalIgnoreCase))
+                        {
+                            imagePath = "pack://application:,,,/Images/btn_WH_CHS_click.png";
+                        }
+                    }
+                    #endregion convertToImagePath
+                }
+                if (imagePath != null)
+                {
+                    result= new ImageSourceConverter().ConvertFromString(imagePath);
+                }               
+            }
+            catch (Exception ex)
+            {
+                result = null;
+                WindowUI.NlogHelper.LogToFile(ex.ToString());
+            }
+            return result;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WindowUITest/Global/loginTypeValueConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WindowUITest
+{
+    /// <summary>
+    /// loginTypeValue
+    /// </summary>
+    public class loginTypeValue
+    {
+        public string TypeValue { get; set; }
+    }
+
+    /// <summary>
+    /// loginTypeValueConverter
+    /// </summary>
+    public class loginTypeValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return new loginTypeValue() { TypeValue= (value ?? "").ToString().Trim() };            
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
