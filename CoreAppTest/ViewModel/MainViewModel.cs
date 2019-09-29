6b028d74b1b34e765089a64e953308b7e94ec8b9
AIMKK <a_5210@126.com>
2019-09-29 17:28:44 +0800

    mainuiesc

--- CoreAppTest/ViewModel/MainViewModel.cs
+++ CoreAppTest/ViewModel/MainViewModel.cs
@@ -1,10 +1,50 @@
-﻿using System;
+﻿using CoreLibrary;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows;
 
 namespace CoreAppTest
 {
-    public class MainViewModel
+    public class MainViewModel: ViewModelBase
     {
+        public RelayCommand<Window> FormLoadedCommand { get; set; }
+
+        public RelayCommand<Window> FormClosedCommand { get; set; }
+
+        public MainViewModel()
+        {
+            //
+            FormLoadedCommand = new RelayCommand<Window>(formLoaded);
+            FormClosedCommand = new RelayCommand<Window>(formClosed);
+        }
+
+        /// <summary>
+        /// formLoaded
+        /// </summary>
+        /// <param name="win"></param>
+        private void formLoaded(Window win)
+        {
+            if (win != null)
+            {
+                //Messenger.Default.Register<TargetViewCreateMessage>(win, MessageTokens.ShowLoginLocationSelectUI, showTargetView);
+
+            }
+        }
+
+        /// <summary>
+        /// formClosed
+        /// </summary>
+        /// <param name="win"></param>
+        private void formClosed(Window win)
+        {
+            if (win != null)
+            {
+                //Messenger.Default.Unregister<TargetViewCreateMessage>(win, MessageTokens.ShowLoginLocationSelectUI, showTargetView);
+                win.Close();
+            }
+        }
     }
 }
